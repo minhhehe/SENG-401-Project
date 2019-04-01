8aa2d752d39dfc1938a3cd04be8b2779c32a6b43
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-01 04:17:32 -0600

    [task2.1.3] Started creating render_model migration.

---
+++ app/Http/Controllers/RenderModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RenderModel;
+use Illuminate\Http\Request;
+
+class RenderModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RenderModel  $renderModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RenderModel $renderModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RenderModel  $renderModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RenderModel $renderModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RenderModel  $renderModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RenderModel $renderModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RenderModel  $renderModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RenderModel $renderModel)
+    {
+        //
+    }
+}
