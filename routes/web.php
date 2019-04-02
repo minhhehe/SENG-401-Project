5742acfbe5e3bab6c4005de28d8e824844e4de5e
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-04-02 13:39:51 -0600

    reset homepage to default welcome page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('laravel-default-welcome');
 });
 
 Route::get('/storage/{filename}', 'StorageController@sendFile');
