6b74b9414edca3e69949eaf9198eda98b83efa16
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-04-04 13:24:22 -0600

    fixing root view route

--- resources/views/welcome_visitor.blade.php
+++ resources/views/welcome_visitor.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.layout_welcome')
 
 @section('logo')
-<div><img src="{{ asset('img/CADA_logo.jpg') }}"></div>
 @endsection
 
 @section('title')
-<div>CADA Realistic Automotive Project</div>
+<div>SENG 401 Project Visitor Landing Page</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome_to_CADA');
+    return view('welcome_visitor');
 });
 
 Route::get('/car_example', function() {
