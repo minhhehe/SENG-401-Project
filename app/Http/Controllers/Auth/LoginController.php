5177f72f617736cf616e628c2fb7a09b2132a45a
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-04-05 12:01:44 -0600

    Updating links to redirect to model select screen. Adding username to topright

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/select';
 
     /**
      * Create a new controller instance.
