633a2d3544877314673b604cd7ab7d5b8b7b1b88
Alex Mah <mahalex88@gmail.com>
2019-04-05 23:03:20 -0600

    Migrations for customer. Probably works

--- database/migrations/2019_04_05_234935_create_customers_table.php
+++ database/migrations/2019_04_05_234935_create_customers_table.php
@@ -20,22 +20,22 @@ class CreateCustomersTable extends Migration
             $table->date('dob');//DOB
             $table->string('gender');//Gender
             $table->string('address');//Address
-            $table->string('phoneNum')//Phone Number
-            $table->string('email')->unique();;//Email
-            $table->string('billingInfo')//Billing info
-            $table->string('lastVehiclePurchased')//Last vehicle purchaced
+            $table->string('phoneNum'); //Phone Number
+            $table->string('email')->unique();//Email
+            $table->string('billingInfo'); //Billing info
+            $table->string('lastVehiclePurchased'); //Last vehicle purchaced
             $table->date('lastVehicleYear');//Last Vehicle year
-            $table->string('lastInterior')//Last vehicle interior Colour
-            $table->string('lastExterior')//Last vehicle exterior Colour
-            $table->string('lastRim')//Last vehicle rim Colour
-            $table->string('lastGlass')//Last vehicle glass Colour
-            $table->string('desiredModel')//Desired model
-            $table->string('desiredInterior')//desired interior colour
-            $table->string('desiredExterior')//desired exterior colour
-            $table->string('desiredRim')//Desired Rim colour
-            $table->string('desiredGlass')//Desired Glass Colour
-            $table->string('desiredPrice')//Desired Price Range
-            $table->string('paymentMethod')//Desired Payment Method
+            $table->string('lastInterior'); //Last vehicle interior Colour
+            $table->string('lastExterior'); //Last vehicle exterior Colour
+            $table->string('lastRim'); //Last vehicle rim Colour
+            $table->string('lastGlass'); //Last vehicle glass Colour
+            $table->string('desiredModel'); //Desired model
+            $table->string('desiredInterior'); //desired interior colour
+            $table->string('desiredExterior'); //desired exterior colour
+            $table->string('desiredRim'); //Desired Rim colour
+            $table->string('desiredGlass'); //Desired Glass Colour
+            $table->string('desiredPrice'); //Desired Price Range
+            $table->string('paymentMethod'); //Desired Payment Method
             $table->timestamps();
         });
     }
