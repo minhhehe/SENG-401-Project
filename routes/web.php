c447a21202f47cbb22343c6d90232788e534e759
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-05 19:19:47 -0600

    [task1.6.1] Finished content of submitted page.

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/select', 'HomeController@select');
+
+Route::get('/submitted', function() {
+    return view('submitted');
+});
