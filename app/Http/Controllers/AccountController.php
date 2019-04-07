8194be2aaceedf77ab6f83c2d80343d95851f459
Shishir <shishir2812@gmail.com>
2019-04-06 23:51:34 -0600

    AccountsController added

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        return view('accounts.index',compact['user']);
+    }
+
+}
