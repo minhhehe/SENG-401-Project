7880c9adb84caac6624dc6440ab65fab4ddbe022
Shishir <shishir2812@gmail.com>
2019-04-09 12:19:07 -0600

    rendered model authorization

--- app/Http/Controllers/RenderedmodelController.php
+++ app/Http/Controllers/RenderedmodelController.php
@@ -14,7 +14,11 @@ class RenderedModelController extends Controller
    *
    * @return \Illuminate\Http\Response
   */
+  public function __construct()
+  {
+      $this->middleware('auth');
 
+  }
   public function index()
   {
       //
