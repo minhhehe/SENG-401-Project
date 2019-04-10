f7046bab3b3f817dd142402cb7f0c7af6f156728
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-10 15:13:57 -0600

    Fixed indenting (who did that wrong in the first place, you heathens)

--- resources/views/render_model.blade.php
+++ resources/views/render_model.blade.php
@@ -1,126 +1,105 @@
 <?php
 	$loadFull = ($renderedModel->file_name == "ferrari.glb");
- ?>
-
-
+	?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title> FK BS 2019 </title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-	<script src="{{ asset('js/app.js') }}"></script>
-
-	<style>
-	body {
-		/* font-family: Monospace; */
-		background-color: #000;
-		color: #000;
-		margin: 0px;
-		overflow: hidden;
-	}
-	#info {
-		position: absolute;
-		top: 80px;
-		width: 100%;
-		text-align: center;
-		z-index: 100;
-	}
-	#info a {
-		color: blue;
-		font-weight: bold;
-	}
-	 .topRight {
-		 position: absolute;
-		   top: 8px;
-		   right: 16px;
-		   font-size: 18px;
-	 }
-
-	</style>
-</head>
-
-<body>
-	<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+	<head>
+		<title> FK BS 2019 </title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+		<script src="{{ asset('js/app.js') }}"></script>
+		<style>
+			body {
+			/* font-family: Monospace; */
+			background-color: #000;
+			color: #000;
+			margin: 0px;
+			overflow: hidden;
+			}
+			#info {
+			position: absolute;
+			top: 80px;
+			width: 100%;
+			text-align: center;
+			z-index: 100;
+			}
+			#info a {
+			color: blue;
+			font-weight: bold;
+			}
+			.topRight {
+			position: absolute;
+			top: 8px;
+			right: 16px;
+			font-size: 18px;
+			}
+		</style>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
 		<!-- Left Side Of Navbar -->
 		<a class="navbar-brand" href="{{ url('/') }}">
-				<!-- {{ config('app.name', 'Laravel') }} -->
-				FKBmb Background Simulator 2019
+			<!-- {{ config('app.name', 'Laravel') }} -->
+			FKBmb Background Simulator 2019
 		</a>
 		<ul class="navbar-nav mr-auto">
-
 		</ul>
-
-			<div class="container">
-
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-							<span class="navbar-toggler-icon"></span>
-					</button>
-
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<!-- Right Side Of Navbar -->
-							<ul class="navbar-nav ml-auto">
-									<!-- Authentication Links -->
-									@guest
-								<li class="nav-item">
-										<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-								</li>
-								@if (Route::has('register'))
-										<li class="nav-item">
-												<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-										</li>
-								@endif
-						@else
-								<li class="nav-item dropdown">
-										<a title = "User Information" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-												{{ Auth::user()->fname }} {{ Auth::user()->lname}} <span class="caret"></span>
-										</a>
-
-										<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-											<a class="dropdown-item" href="{{ url('/home') }}">Your Account<a>
-												<div class="h-divider"></div>
-												<a class="dropdown-item" href="{{ route('logout') }}"
-													 onclick="event.preventDefault();
-																				 document.getElementById('logout-form').submit();">
-														{{ __('Logout') }}
-												</a>
-
-												<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-													@csrf
-												</form>
-										</div>
-								</li>
-						@endguest
+		<div class="container">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<!-- Right Side Of Navbar -->
+				<ul class="navbar-nav ml-auto">
+					<!-- Authentication Links -->
+					@guest
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+					@if (Route::has('register'))
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+					</li>
+					@endif
+					@else
+					<li class="nav-item dropdown">
+						<a title = "User Information" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+						{{ Auth::user()->fname }} {{ Auth::user()->lname}} <span class="caret"></span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ url('/home') }}">Your Account
+							<a>
+								<div class="h-divider"></div>
+							<a class="dropdown-item" href="{{ route('logout') }}"
+								onclick="event.preventDefault();
+								document.getElementById('logout-form').submit();">
+							{{ __('Logout') }}
+							</a>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
+					@endguest
 				</ul>
 			</div>
 		</div>
-
 		<div id="container"></div>
-
 		<button class="btn btn-secondary topRight" onclick="document.location.href='{{ url('/submitted') }}'"> Submit </button>
-	</div>
-
-	<script src="{{ asset('js/three.min.js') }}"></script>
-
-	<script src="{{ asset('js/loaders/DRACOLoader.js') }}"></script>
-	<script src="{{ asset('js/loaders/GLTFLoader.js') }}"></script>
-
-	<script src="{{ asset('js/pmrem/PMREMGenerator.js') }}"></script>
-	<script src="{{ asset('js/pmrem/PMREMCubeUVPacker.js') }}"></script>
-
-	<script src="{{ asset('js/Car.js') }}"></script>
-
-	<script src="{{ asset('js/WebGL.js') }}"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+		</div>
+		<script src="{{ asset('js/three.min.js') }}"></script>
+		<script src="{{ asset('js/loaders/DRACOLoader.js') }}"></script>
+		<script src="{{ asset('js/loaders/GLTFLoader.js') }}"></script>
+		<script src="{{ asset('js/pmrem/PMREMGenerator.js') }}"></script>
+		<script src="{{ asset('js/pmrem/PMREMCubeUVPacker.js') }}"></script>
+		<script src="{{ asset('js/Car.js') }}"></script>
+		<script src="{{ asset('js/WebGL.js') }}"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
-
 			if ( WEBGL.isWebGLAvailable() === false ) {
 
 				document.body.appendChild( WEBGL.getWebGLErrorMessage() );
