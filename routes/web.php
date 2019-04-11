9b7d50228ad1f89ff5f83d3d5dfbd620f853b918
Shishir <shishir2812@gmail.com>
2019-04-11 03:29:38 -0600

    Authorization tentatively done

--- routes/web.php
+++ routes/web.php
@@ -23,24 +23,25 @@ Route::get('/selectedModel/{id}', 'HomeController@index');
 
 Route::get('/flickr', function() {
     return view('flickr_API_test');
-});
+})->middleware('auth');
 
-Route::get('/render_model/{id}', 'RenderedModelController@show');
+Route::get('/render_model/{id}', 'RenderedModelController@show')->middleware('auth');
 
-Route::get('/render_model_backup/{id}', 'RenderedModelController@showBackup');
+Route::get('/render_model_backup/{id}', 'RenderedModelController@showBackup')->middleware('auth');
 
 Route::get('/storage/{filename}', 'StorageController@sendFile');
+//Route::get('/storage/{filename}', 'StorageController@sendFile')->middleware('auth');
 
 Auth::routes();
 
 
-Route::get('/home', 'AccountController@index');
+Route::get('/home', 'AccountController@index')->middleware('auth');
 Route::patch('/home', 'AccountController@update');
 Route::get('/select', 'HomeController@select');
 
 Route::get('/submitted', function() {
     return view('submitted');
-});
+})->middleware('auth');
 
 // HOTFIX
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
