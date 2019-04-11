ce250d75fff1e2e5868748e97a988b981fe7ddcc
Shishir <shishir2812@gmail.com>
2019-04-11 02:32:41 -0600

    middlewares added

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,31 @@ Route::get('/', function () {
     return redirect('/select');
 });
 
+Route::get('/selectedModel/{id}', 'HomeController@index')->middleware('auth');
+// Route::get('/', function () {
+//     return redirect('/select');
+// });
+
 Route::get('/flickr', function() {
     return view('flickr_API_test');
-});
+})->middleware('auth');
 
-Route::get('/render_model/{id}', 'RenderedModelController@show');
+Route::get('/render_model/{id}', 'RenderedModelController@show')->middleware('auth');
 
-Route::get('/storage/{filename}', 'StorageController@sendFile');
+Route::get('/render_model_backup/{id}', 'RenderedModelController@showBackup')->middleware('auth');
+
+Route::get('/storage/{filename}', 'StorageController@sendFile')->middleware('auth');
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
-//Route::get('/home', 'AccountController@index')->name('home');
-//Route::patch('/home', 'AccountController@update');
+
+Route::get('/home', 'AccountController@index')->middleware('auth');
+Route::patch('/home', 'AccountController@update')->middleware('auth');
 Route::get('/select', 'HomeController@select');
 
 Route::get('/submitted', function() {
     return view('submitted');
-});
+})->middleware('auth');
 
 // HOTFIX
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
